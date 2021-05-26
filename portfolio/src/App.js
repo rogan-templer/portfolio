caf621e8ab0d558368b0ad82121e5d58fc59d825
Rogan-Templer <rmtempler@gmail.com>
2021-05-26 16:34:13 +1200

    App.js set up

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello!</h1>
+      <p>My name is Rogan. I am a Fullstack Software and Web Developer</p>
+      <p>I have gathered together some projects here that I have been working on.</p>
+      </div>
+    )
+  }
+}
+
+export default App
