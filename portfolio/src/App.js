6c51a47b2d088197ebdff7e0655c2fc1cdd28cd3
Rogan-Templer <rmtempler@gmail.com>
2021-05-26 20:23:19 +1200

    bio section added to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false }
+  }
+
+
   render() {
     return (
       <div>
       <h1>Hello!</h1>
       <p>My name is Rogan. I am a Fullstack Software and Web Developer</p>
       <p>I have gathered together some projects here that I have been working on.</p>
+      {
+        this.state.displayBio ? (
+          <div>
+          <p>I live in Auckland and have recently pivoted into tech after a prior career as an Underwriter in the Life and disability Insurance world.</p>
+          <p>When I am not working I spend time with my family and have been developing a healthy obsession with surfing.</p>
+          </div>
+          ) : null
+      }
       </div>
     )
   }
