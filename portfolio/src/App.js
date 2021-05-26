122482403e5418508169793515e1a8d9785ff2d2
Rogan-Templer <rmtempler@gmail.com>
2021-05-27 11:16:17 +1200

    app.js refactored

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,16 +2,9 @@ import React, { Component } from "react";
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { displayBio: false }
+  state = { displayBio: false }
 
-    // console.log('component this', this);
-    
-    this.toggleDisplayBio = this.toggleDisplayBio.bind(this)
-  }
-
-  toggleDisplayBio() {
+  toggleDisplayBio = () => {
     this.setState({ displayBio: !this.state.displayBio })
   }
 
