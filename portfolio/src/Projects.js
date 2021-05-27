ad132c06b334c4e6a8e8220f7c176efc8bfe752f
Rogan-Templer <rmtempler@gmail.com>
2021-05-27 12:16:53 +1200

    Project.js updated to display projects

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -1,1 +1,21 @@
 import React, { Component } from "react";
+import PROJECTS from './data/projects'
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+      <h2>Projects</h2>
+      <div>
+        {
+          PROJECTS.map(PROJECT => {
+            return(
+              <div>{PROJECT.title}</div>
+            )
+          })
+        }
+      </div>
+      </div>
+    )
+  }
+}
