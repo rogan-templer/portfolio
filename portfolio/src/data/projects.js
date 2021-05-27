8942a3d53acbdc7507d92d1bbb4028a30b0fbc7b
Rogan-Templer <rmtempler@gmail.com>
2021-05-27 12:06:15 +1200

    projects.js data file and folder set up

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,18 @@
+import project1 from '../assets/project1.png'
+import project2 from '../assets/project2.png'
+import project3 from '../assets/project3.png'
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: 'Example HTML Blog',
+    description: 'A simple Blog website I created with HTML and CSS that is mobile friendly.',
+    link: 'https://rogan-templer.github.io/',
+    image: project1
+  },
+  {
+
+  }
+];
+
+exports default PROJECTS;
