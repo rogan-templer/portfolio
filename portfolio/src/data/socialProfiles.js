5989496c212cc36b249916da8a79ddb1fe64da16
Rogan-Templer <rmtempler@gmail.com>
2021-05-27 17:36:08 +1200

    social profiles data file created

---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,23 @@
+import email from '../assets/email_icon.png'
+import github from '../assets/github_icon.png'
+import linkedin from '../assets/linkedin_icon.png'
+
+const SOCIAL = [
+  {
+    id: 1,
+    title: 'E-mail',
+    image: email
+  },
+  {
+    id: 2,
+    title: 'Github',
+    image: github
+  },
+  {
+    id: 1,
+    title: 'LinkedIn',
+    image: linkedin
+  }
+]
+
+export default SOCIAL
