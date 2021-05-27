93bab72762a9d9a2083aa63a546faa284e34f118
Rogan-Templer <rmtempler@gmail.com>
2021-05-27 20:40:26 +1200

    updated details in the socialprofiles.js

--- portfolio/src/data/socialProfiles.js
+++ portfolio/src/data/socialProfiles.js
@@ -1,22 +1,22 @@
-import email from '../assets/email_icon.png'
-import github from '../assets/github_icon.png'
-import linkedin from '../assets/linkedin_icon.png'
+import emailIcon from '../assets/email_icon.png'
+import githubIcon from '../assets/github_icon.png'
+import linkedinIcon from '../assets/linkedin_icon.png'
 
 const SOCIAL = [
   {
     id: 1,
-    title: 'E-mail',
-    image: email
+    link: 'rmtempler@gmail.com',
+    image: emailIcon
   },
   {
     id: 2,
-    title: 'Github',
-    image: github
+    link: 'https://github.com/rogan-templer',
+    image: githubIcon
   },
   {
     id: 1,
-    title: 'LinkedIn',
-    image: linkedin
+    link: 'https://www.linkedin.com/in/rogan-templer-327158167/',
+    image: linkedinIcon
   }
 ]
 
